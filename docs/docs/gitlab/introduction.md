23034da9514b2b09a769a28b3663d3e689e37493
Mathieu herrmann <mathieu.herrmann@devoteam.com>
2023-12-28 10:39:28 +0100

    docs(docusaurus): add content to the documentation

--- docs/docs/gitlab/introduction.md
+++ docs/docs/gitlab/introduction.md
@@ -3,3 +3,10 @@ sidebar_position: 1
 ---
 
 # Introduction
+
+You will be able to find the documentation and explaination about the all the Gitlab Folder of the repository.
+
+First of all, I defined term like 
+- [Job](./jobs.md)
+- [Base](./base.md)
+- [Pipeline](./pipelines.md)
--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -4,11 +4,11 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'My Template',
-  tagline: 'Dinosaurs are cool',
+  tagline: '',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'http://localhost:3000',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -36,8 +36,8 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/MathieuHerrmann/My_Templates/tree/main/docs',
         },
         blog: false,
         // blog: {
